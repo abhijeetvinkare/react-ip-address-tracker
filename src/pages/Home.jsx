abf4ae2ac05a5b23663a2b5ce2d7eb41d38116ef
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-01-01 23:46:01 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="main-container">
+        <div>
+            <h1>IP Address Tracker</h1>
+        </div>
+        <div>
+            <h1>Maps</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
