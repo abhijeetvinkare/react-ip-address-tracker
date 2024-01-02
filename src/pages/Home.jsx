2238b32fa1cf21e99e79738240a9d8660c6490fc
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-01-02 23:26:58 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import LocationMarker from "./LocationMarker";
 
 function Home() {
   return (
     <div className="main-container">
-        <div>
-            <h1>IP Address Tracker</h1>
-        </div>
-        <div>
-            <h1>Maps</h1>
-        </div>
+      <div>
+        <h1>IP Address Tracker</h1>
+      </div>
+      <div>
+        <MapContainer
+          center={{ lat: 51.505, lng: -0.09 }}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <LocationMarker />
+        </MapContainer>
+        ,
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
