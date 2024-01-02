1a66253260710d72cc9bb73a353a5e9a4673162e
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-01-02 23:46:35 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import "./Home.css";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
-import LocationMarker from "./LocationMarker";
 
 function Home() {
+  const position = [51.505, -0.09]
   return (
     <div className="main-container">
       <div>
         <h1>IP Address Tracker</h1>
       </div>
       <div>
-        <MapContainer
-          center={{ lat: 51.505, lng: -0.09 }}
-          zoom={13}
-          scrollWheelZoom={false}
-        >
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <LocationMarker />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
         </MapContainer>
-        ,
       </div>
     </div>
   );
