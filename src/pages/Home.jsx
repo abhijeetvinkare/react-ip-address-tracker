8c81f2f0ba679ef6308641f0dfc804d68affcd5d
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-01-06 21:38:19 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { IoIosArrowForward } from "react-icons/io";
 import axios from "axios";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function Home() {
   const [ipInfo, setIpInfo] = useState(null);
@@ -11,6 +13,7 @@ function Home() {
     lat: 51.505,
     lon: -0.09,
   });
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -102,41 +105,58 @@ function Home() {
           </div>
         </>
       )}
-
-
-{!ipInfo && ( <>
-      <div className="result-container">
-        <div className="result-container-child">
-          <span className="title-span">IP ADDRESS</span>
-          <span className="info-span">127.101.131.11</span>
-        </div>
-        <div className="result-container-child">
-          <span className="title-span">LOCATION</span>
-          <span className="info-span">New York, USA</span>
-        </div>
-        <div className="result-container-child">
-          <span className="title-span">TIMEZONE</span>
-          <span className="info-span">UTC - 5:00</span>
-        </div>
-        <div className="result-container-child">
-          <span className="title-span">ISP</span>
-          <span className="info-span">SpaceX</span>
-        </div>
-      </div>
-      <div className="map-container">
-        <MapContainer center={dummyPosition} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={dummyPosition}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
-      </>)}
+      {!ipInfo && (
+        <>
+          <div className="result-container">
+            <div className="result-container-child">
+              <span className="title-span">IP ADDRESS</span>
+              <span className="info-span">127.101.131.11</span>
+            </div>
+            <div className="result-container-child">
+              <span className="title-span">LOCATION</span>
+              <span className="info-span">New York, USA</span>
+            </div>
+            <div className="result-container-child">
+              <span className="title-span">TIMEZONE</span>
+              <span className="info-span">UTC - 5:00</span>
+            </div>
+            <div className="result-container-child">
+              <span className="title-span">ISP</span>
+              <span className="info-span">SpaceX</span>
+            </div>
+          </div>
+          <div className="map-container">
+            <MapContainer
+              center={dummyPosition}
+              zoom={13}
+              scrollWheelZoom={false}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={dummyPosition}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+        </>
+      )}
+      {loading ? (
+        <Backdrop
+          sx={{
+            color: "#ffffff",
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+          }}
+          open
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
